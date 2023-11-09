5813b39cf52f438a051b4f72b7cb4f5324d0e848
Abhay Bhatia <abhay1732002@gmail.com>
2023-11-09 14:17:56 +0530

    major change

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -6,7 +6,10 @@ export default function Home() {
         <div className="home-container">
             <h1>You got the travel plans, we got the travel vans.</h1>
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Link to="vans">Find your van</Link>
+            <div className="home-p-a-conatiner">
+                <Link className="home-link" to="vans">Find your van</Link>
+                <Link className="home-link" to="host">host your van</Link>
+            </div>
         </div>
     )
 };
